1210c6354a5b09045c17bd0fa93c78d3fd2c1755
Mahdi <mkfard@outlook.com>
2024-04-02 15:44:50 +0330

    Fix bug in building flags.

--- lib/src/flag_builder.dart
+++ lib/src/flag_builder.dart
@@ -36,100 +36,100 @@ int _flag({
 }) {
   var value = 0;
   if (b00) {
-    value &= (1 << 0);
+    value |= (1 << 0);
   }
   if (b01) {
-    value &= (1 << 1);
+    value |= (1 << 1);
   }
   if (b02) {
-    value &= (1 << 2);
+    value |= (1 << 2);
   }
   if (b03) {
-    value &= (1 << 3);
+    value |= (1 << 3);
   }
   if (b04) {
-    value &= (1 << 4);
+    value |= (1 << 4);
   }
   if (b05) {
-    value &= (1 << 5);
+    value |= (1 << 5);
   }
   if (b06) {
-    value &= (1 << 6);
+    value |= (1 << 6);
   }
   if (b07) {
-    value &= (1 << 7);
+    value |= (1 << 7);
   }
   if (b08) {
-    value &= (1 << 8);
+    value |= (1 << 8);
   }
   if (b09) {
-    value &= (1 << 9);
+    value |= (1 << 9);
   }
   if (b10) {
-    value &= (1 << 10);
+    value |= (1 << 10);
   }
   if (b11) {
-    value &= (1 << 11);
+    value |= (1 << 11);
   }
   if (b12) {
-    value &= (1 << 12);
+    value |= (1 << 12);
   }
   if (b13) {
-    value &= (1 << 13);
+    value |= (1 << 13);
   }
   if (b14) {
-    value &= (1 << 14);
+    value |= (1 << 14);
   }
   if (b15) {
-    value &= (1 << 15);
+    value |= (1 << 15);
   }
   if (b16) {
-    value &= (1 << 16);
+    value |= (1 << 16);
   }
   if (b17) {
-    value &= (1 << 17);
+    value |= (1 << 17);
   }
   if (b18) {
-    value &= (1 << 18);
+    value |= (1 << 18);
   }
   if (b19) {
-    value &= (1 << 19);
+    value |= (1 << 19);
   }
   if (b20) {
-    value &= (1 << 20);
+    value |= (1 << 20);
   }
   if (b21) {
-    value &= (1 << 21);
+    value |= (1 << 21);
   }
   if (b22) {
-    value &= (1 << 22);
+    value |= (1 << 22);
   }
   if (b23) {
-    value &= (1 << 23);
+    value |= (1 << 23);
   }
   if (b24) {
-    value &= (1 << 24);
+    value |= (1 << 24);
   }
   if (b25) {
-    value &= (1 << 25);
+    value |= (1 << 25);
   }
   if (b26) {
-    value &= (1 << 26);
+    value |= (1 << 26);
   }
   if (b27) {
-    value &= (1 << 27);
+    value |= (1 << 27);
   }
   if (b28) {
-    value &= (1 << 28);
+    value |= (1 << 28);
   }
   if (b29) {
-    value &= (1 << 29);
+    value |= (1 << 29);
   }
   if (b30) {
-    value &= (1 << 30);
+    value |= (1 << 30);
   }
   if (b31) {
-    value &= (1 << 31);
+    value |= (1 << 31);
   }
 
   return value;
