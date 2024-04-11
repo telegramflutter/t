1a6c5f923686099712d23a4ca4735e4cc5e06a10
Mahdi <mkfard@outlook.com>
2024-04-12 00:45:34 +0330

    Update the example.

--- example/main.dart
+++ example/main.dart
@@ -1,21 +1,52 @@
+import 'dart:io';
+
 import 'package:t/t.dart' as t;
 
-void main(List<String> arguments) {
-  final buffer = <int>[];
-  final q = t.InitConnection(
-    apiId: 123456,
-    deviceModel: "Windows",
+void main(List<String> arguments) async {
+  const apiId = 0;
+  const apiHash = '';
+  const phoneNumber = '';
+
+  final t.Client? client = null;
+
+  final cfg = await client!.initConnection<t.Config>(
+    apiId: apiId,
+    deviceModel: "Galaxy S24",
     appVersion: "1.0.0",
-    systemVersion: "10",
-    systemLangCode: "fa",
-    langCode: "fa",
+    systemVersion: "Android",
+    systemLangCode: "en",
+    langCode: "en",
     langPack: "",
     proxy: null,
     params: null,
     query: t.HelpGetConfig(),
   );
 
-  q.serialize(buffer);
+  print('Config: $cfg');
 
-  print(buffer);
+  final sendCodeResponse = await client.auth.sendCode(
+    phoneNumber: phoneNumber,
+    apiId: apiId,
+    apiHash: apiHash,
+    settings: const t.CodeSettings(
+      allowFlashcall: false,
+      currentNumber: false,
+      allowAppHash: false,
+      allowMissedCall: false,
+      allowFirebase: false,
+      appSandbox: false,
+    ),
+  );
+
+  final phoneCode = stdin.readLineSync();
+
+  final sendCode = sendCodeResponse.result as t.AuthSentCode;
+
+  final signInResponse = await client.auth.signIn(
+    phoneNumber: phoneNumber,
+    phoneCodeHash: sendCode.phoneCodeHash,
+    phoneCode: phoneCode,
+  );
+
+  print('SignIn: $signInResponse');
 }
