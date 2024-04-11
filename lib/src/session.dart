edb1055a550b5f3faa27616b5bdd5c223e47f77d
Mahdi <mkfard@outlook.com>
2024-04-12 02:07:29 +0330

    Session serialize/deserialize to/from JSON.

--- lib/src/session.dart
+++ lib/src/session.dart
@@ -10,6 +10,22 @@ class Session {
     this.seqno = 0,
   });
 
+  /// Creates an instance of [Session] from JSON.
+  factory Session.fromJson(Map<String, dynamic> data) {
+    final id = data['id'] as int;
+    final authKeyJson = Map<String, dynamic>.from(data['authKey']);
+    final authKey = AuthorizationKey.fromJson(authKeyJson);
+    final seqno = data['seqno'] as int;
+    final lastSentMessageId = data['lastSentMessageId'] as int;
+
+    return Session(
+      id: id,
+      authKey: authKey,
+      seqno: seqno,
+      lastSentMessageId: lastSentMessageId,
+    );
+  }
+
   /// Session id.
   final int id;
 
@@ -21,4 +37,12 @@ class Session {
 
   /// Authorization information.
   final AuthorizationKey authKey;
+
+  /// Converts this instance to JSON.
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'authKey': authKey.toJson(),
+        'seqno': seqno,
+        'lastSentMessageId': lastSentMessageId,
+      };
 }
